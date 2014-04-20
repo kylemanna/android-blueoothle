1e75e4c2e1e9c3ca87f9515294407c8008275044
Mark Allison <markal@blinkbox.com>
2014-04-20 15:19:55 +0100

    Code for Part3

--- src/main/java/com/stylingandroid/ble/BleActivity.java
+++ src/main/java/com/stylingandroid/ble/BleActivity.java
@@ -1,6 +1,8 @@
 package com.stylingandroid.ble;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.bluetooth.BluetoothAdapter;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
@@ -9,16 +11,27 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.lang.ref.WeakReference;
 
 
-public class BleActivity extends Activity {
+public class BleActivity extends Activity implements DeviceListFragment.OnDeviceListFragmentInteractionListener {
 	public static final String TAG = "BluetoothLE";
+	private final int ENABLE_BT = 1;
+
 	private final Messenger mMessenger;
 	private Intent mServiceIntent;
 	private Messenger mService = null;
+	private BleService.State mState = BleService.State.UNKNOWN;
+
+	private MenuItem mRefreshItem = null;
+
+	private DeviceListFragment mDeviceList = DeviceListFragment.newInstance();
+
 	private ServiceConnection mConnection = new ServiceConnection() {
 		@Override
 		public void onServiceConnected(ComponentName name, IBinder service) {
@@ -53,6 +66,9 @@ public class BleActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ble);
 		mServiceIntent = new Intent(this, BleService.class);
+		FragmentTransaction tx = getFragmentManager().beginTransaction();
+		tx.add(R.id.main_content, mDeviceList);
+		tx.commit();
 	}
 
 	@Override
@@ -80,6 +96,59 @@ public class BleActivity extends Activity {
 		bindService(mServiceIntent, mConnection, BIND_AUTO_CREATE);
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		mRefreshItem = menu.findItem(R.id.action_refresh);
+		//mDeviceListFragment = DeviceListFragment.newInstance(null);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_refresh) {
+			if (mService != null) {
+				startScan();
+			}
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void startScan() {
+		mRefreshItem.setEnabled(false);
+		mDeviceList.setDevices(this, null);
+		mDeviceList.setScanning(true);
+		Message msg = Message.obtain(null, BleService.MSG_START_SCAN);
+		if (msg != null) {
+			try {
+				mService.send(msg);
+			} catch (RemoteException e) {
+				Log.w(TAG, "Lost connection to service", e);
+				unbindService(mConnection);
+			}
+		}
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		if (mRefreshItem != null) {
+			mRefreshItem.setEnabled(mState == BleService.State.IDLE || mState == BleService.State.UNKNOWN);
+		}
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public void onDeviceListFragmentInteraction(String macAddress) {
+
+	}
+
 	private static class IncomingHandler extends Handler {
 		private final WeakReference<BleActivity> mActivity;
 
@@ -91,9 +160,53 @@ public class BleActivity extends Activity {
 		public void handleMessage(Message msg) {
 			BleActivity activity = mActivity.get();
 			if (activity != null) {
-				//TODO: Do something
+				switch (msg.what) {
+					case BleService.MSG_STATE_CHANGED:
+						activity.stateChanged(BleService.State.values()[msg.arg1]);
+						break;
+					case BleService.MSG_DEVICE_FOUND:
+						Bundle data = msg.getData();
+						if (data != null && data.containsKey(BleService.KEY_MAC_ADDRESSES)) {
+							activity.mDeviceList.setDevices(activity, data.getStringArray(BleService.KEY_MAC_ADDRESSES));
+						}
+						break;
+				}
 			}
 			super.handleMessage(msg);
+		}
+	}
+
+	private void stateChanged(BleService.State newState) {
+		mState = newState;
+		switch (mState) {
+			case SCANNING:
+				mRefreshItem.setEnabled(true);
+				mDeviceList.setScanning(true);
+				break;
+			case BLUETOOTH_OFF:
+				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				startActivityForResult(enableBtIntent, ENABLE_BT);
+				break;
+			case IDLE:
+				mRefreshItem.setEnabled(true);
+				mDeviceList.setScanning(false);
+				break;
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == ENABLE_BT) {
+			if (resultCode == RESULT_OK) {
+				startScan();
+			} else {
+				//The user has elected not to turn on
+				//Bluetooth. There's nothing we can do
+				//without it, so let's finish().
+				finish();
+			}
+		} else {
+			super.onActivityResult(requestCode, resultCode, data);
 		}
 	}
 }
--- src/main/java/com/stylingandroid/ble/BleService.java
+++ src/main/java/com/stylingandroid/ble/BleService.java
@@ -1,26 +1,57 @@
 package com.stylingandroid.ble;
 
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
 
 import java.lang.ref.WeakReference;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
-public class BleService extends Service {
+public class BleService extends Service implements BluetoothAdapter.LeScanCallback {
 	public static final String TAG = "BleService";
 	static final int MSG_REGISTER = 1;
 	static final int MSG_UNREGISTER = 2;
+	static final int MSG_START_SCAN = 3;
+	static final int MSG_STATE_CHANGED = 4;
+	static final int MSG_DEVICE_FOUND = 5;
+
+	private static final long SCAN_PERIOD = 3000;
+
+	public static final String KEY_MAC_ADDRESSES = "KEY_MAC_ADDRESSES";
+
+	private final IncomingHandler mHandler;
 	private final Messenger mMessenger;
 	private final List<Messenger> mClients = new LinkedList<Messenger>();
+	private final Map<String, BluetoothDevice> mDevices = new HashMap<String, BluetoothDevice>();
+
+	public enum State {
+		UNKNOWN,
+		IDLE,
+		SCANNING,
+		BLUETOOTH_OFF,
+		CONNECTING,
+		CONNECTED,
+		DISCONNECTING
+	}
+
+	private BluetoothAdapter mBluetooth = null;
+	private State mState = State.UNKNOWN;
 
 	public BleService() {
-		mMessenger = new Messenger(new IncomingHandler(this));
+		mHandler = new IncomingHandler(this);
+		mMessenger = new Messenger(mHandler);
 	}
 
 	@Override
@@ -48,10 +79,91 @@ public class BleService extends Service {
 						service.mClients.remove(msg.replyTo);
 						Log.d(TAG, "Unegistered");
 						break;
+					case MSG_START_SCAN:
+						service.startScan();
+						Log.d(TAG, "Start Scan");
+						break;
 					default:
 						super.handleMessage(msg);
 				}
 			}
 		}
+	}
+
+	private void startScan() {
+		mDevices.clear();
+		setState(State.SCANNING);
+		if (mBluetooth == null) {
+			BluetoothManager bluetoothMgr = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+			mBluetooth = bluetoothMgr.getAdapter();
+		}
+		if (mBluetooth == null || !mBluetooth.isEnabled()) {
+			setState(State.BLUETOOTH_OFF);
+		} else {
+			mHandler.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					if (mState == State.SCANNING) {
+						mBluetooth.stopLeScan(BleService.this);
+						setState(State.IDLE);
+					}
+				}
+			}, SCAN_PERIOD);
+			mBluetooth.startLeScan(this);
+		}
+	}
+
+	@Override
+	public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+		if (device != null && !mDevices.containsValue(device) && device.getName() != null && device.getName().equals("SensorTag")) {
+			mDevices.put(device.getAddress(), device);
+			Message msg = Message.obtain(null, MSG_DEVICE_FOUND);
+			if (msg != null) {
+				Bundle bundle = new Bundle();
+				String[] addresses = mDevices.keySet().toArray(new String[mDevices.size()]);
+				bundle.putStringArray(KEY_MAC_ADDRESSES, addresses);
+				msg.setData(bundle);
+				sendMessage(msg);
+			}
+			Log.d(TAG, "Added " + device.getName() + ": " + device.getAddress());
+		}
+	}
+
+	private void setState(State newState) {
+		if (mState != newState) {
+			mState = newState;
+			Message msg = getStateMessage();
+			if (msg != null) {
+				sendMessage(msg);
+			}
+		}
+	}
+
+	private Message getStateMessage() {
+		Message msg = Message.obtain(null, MSG_STATE_CHANGED);
+		if (msg != null) {
+			msg.arg1 = mState.ordinal();
+		}
+		return msg;
+	}
+
+	private void sendMessage(Message msg) {
+		for (int i = mClients.size() - 1; i >= 0; i--) {
+			Messenger messenger = mClients.get(i);
+			if (!sendMessage(messenger, msg)) {
+				mClients.remove(messenger);
+			}
+		}
+	}
+
+	private boolean sendMessage(Messenger messenger, Message msg) {
+		boolean success = true;
+		try {
+			messenger.send(msg);
+		} catch (RemoteException e) {
+			Log.w(TAG, "Lost connection to client", e);
+			success = false;
+		}
+		return success;
 	}
 }
